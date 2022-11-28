4617ea7b6a0396a9c943291dffb00f2e7c26f42f
LuongHuuLuan <19130131@st.hcmuaf.edu.vn>
2022-11-28 21:46:31 +0700

    api phan tinh

--- LotteryAPI/src/main/java/com/lottery/lotteryapi/controller/ProvinceController.java
+++ LotteryAPI/src/main/java/com/lottery/lotteryapi/controller/ProvinceController.java
@@ -1,4 +1,40 @@
 package com.lottery.lotteryapi.controller;
 
+import com.lottery.lotteryapi.dto.ProvinceDTO;
+import com.lottery.lotteryapi.service.ProvinceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/Province")
 public class ProvinceController {
+
+    @Autowired
+    private ProvinceService provinceService;
+
+    @GetMapping
+    public ResponseEntity<List<ProvinceDTO>> getProvinceToday() {
+        List<ProvinceDTO> results = provinceService.getProvincesToday();
+        if (results.size() != 0) {
+            return new ResponseEntity<>(results, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{date}")
+    public ResponseEntity<List<ProvinceDTO>> getProvinceToday(@PathVariable String date) {
+        List<ProvinceDTO> results = provinceService.getProvinces(date);
+        if (results.size() != 0) {
+            return new ResponseEntity<>(results, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- LotteryAPI/src/main/java/com/lottery/lotteryapi/service/ProvinceService.java
+++ LotteryAPI/src/main/java/com/lottery/lotteryapi/service/ProvinceService.java
@@ -1,10 +1,9 @@
 package com.lottery.lotteryapi.service;
 
-import com.lottery.lotteryapi.dto.LotteryDTO;
-import com.lottery.lotteryapi.dto.ResultDTO;
+import com.lottery.lotteryapi.dto.ProvinceDTO;
 import com.lottery.lotteryapi.entity.DateEntity;
 import com.lottery.lotteryapi.entity.LotteryEntity;
-import com.lottery.lotteryapi.entity.ResultEntity;
+import com.lottery.lotteryapi.entity.ProvinceEntity;
 import com.lottery.lotteryapi.repository.DateRepository;
 import com.lottery.lotteryapi.repository.LotteryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,27 +14,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class LotteryService {
-
-    @Autowired
-    private LotteryRepository lotteryRepository;
+public class ProvinceService {
     @Autowired
     private DateRepository dateRepository;
+    @Autowired
+    private LotteryRepository lotteryRepository;
 
-
-    public List<LotteryDTO> getLotteriesToday() {
+    public List<ProvinceDTO> getProvincesToday() {
         LocalDateTime now = LocalDateTime.now();
         int day = now.getDayOfMonth();
         int month = now.getMonthValue();
         int year = now.getYear();
-        return getLotteries(day + "-" + month + "-" + year);
+        return getProvinces(day + "-" + month + "-" + year);
     }
 
-    public List<LotteryDTO> getLotteries(String date) {
+    public List<ProvinceDTO> getProvinces(String date) {
         int day;
         int month;
         int year;
-        List<LotteryDTO> results = new ArrayList<>();
+        List<ProvinceDTO> results = new ArrayList<>();
         try {
             day = Integer.parseInt(date.split("-")[0]);
             month = Integer.parseInt(date.split("-")[1]);
@@ -43,19 +40,11 @@ public class LotteryService {
             DateEntity dateEntity = dateRepository.findOneByDateAndMonthAndYear(day, month, year);
             List<LotteryEntity> lotteryEntities = lotteryRepository.findByDateAndIsDelete(dateEntity, "FALSE");
             for (LotteryEntity lotteryEntity : lotteryEntities) {
-                LotteryDTO lotteryDTO = new LotteryDTO();
-                lotteryDTO.setId(lotteryEntity.getIdLot());
-                lotteryDTO.setDate(lotteryEntity.getDate().getFullDate());
-                lotteryDTO.setProvince(lotteryEntity.getProvince().getName());
-                List<ResultDTO> lotteryResults = new ArrayList<>();
-                for (ResultEntity resultEntity : lotteryEntity.getResults()) {
-                    ResultDTO resultDTO = new ResultDTO();
-                    resultDTO.setPrizeName(resultEntity.getPrize().getName());
-                    resultDTO.setResults(resultEntity.getResult());
-                    lotteryResults.add(resultDTO);
-                }
-
-                results.add(lotteryDTO);lotteryDTO.setResults(lotteryResults);
+                ProvinceEntity provinceEntity = lotteryEntity.getProvince();
+                ProvinceDTO provinceDTO = new ProvinceDTO();
+                provinceDTO.setIdPro(provinceEntity.getIdPro());
+                provinceDTO.setName(provinceEntity.getName());
+                results.add(provinceDTO);
             }
         } catch (Exception e) {
             e.printStackTrace();
